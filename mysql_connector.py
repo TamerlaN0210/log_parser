6de31cab54772b5daf98e8c068b76c4c4151290f
TamerlaN0210 <timurahmadulin@mail.ru>
2019-01-17 22:26:21 +0500

    удалил ненужный коннектор

--- mysql_connector.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-
-mydb = mysql.connector.connect(host="localhost", user="root", password="", database="logs_data")
-# mycursor = mydb.cursor()
-# mycursor.execute("SELECT * FROM books WHERE `author_id` = (SELECT `id` from authors WHERE `full_name` = 'Федор Михайлович Достоевский')")
-# for item in mycursor:
-#     print(item)
